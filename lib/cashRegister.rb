ea4fd6249150355b10333548cff3ef83801af511
Darius Seals <tristarmediagroupinc@gmail.com>
2018-12-13 13:26:40 -0500

    made final changes to code so that correct output

--- lib/cashRegister.rb
+++ lib/cashRegister.rb
@@ -1,63 +1,51 @@
 
-
 class CashRegister
-
-
-
-  CASH_DICTIONARY =   {"ONEHUNDRED "=> 100.00,
-                       "TWENTY "=> 20.00,
-                       "TEN "=> 10.00,
-                       "FIVE "=> 5.00,
-                       "ONE "=> 1.00,
-                       "QUARTER "=> 0.25,
-                       "DIME "=> 0.10,
-                       "NICKEL "=> 0.05,
-                       "PENNY "=> 0.01 }.freeze
+  CASH_DICTIONARY = {
+                     "ONE HUNDRED" => 100,
+                     "TWENTY" => 20,
+                     "TEN" => 10,
+                     "FIVE" => 5,
+                     "ONE" => 1,
+                     "QUARTER" => 0.25,
+                     "DIME" => 0.10,
+                     "NICKEL" => 0.05,
+                     "PENNY" => 0.01}.freeze
 
   def self.check_cash_register(price, cash, cashInDrawer)
     change_from_purchase = (cash - price).round(2)
-    total_up = cashInDrawer.reduce(0){|sum,(name, number)| sum + number}.round(2)
+    total_up = cashInDrawer.reduce(0) { |sum, (name, number)| sum + number }.round(2)
     cid = cashInDrawer.reverse
-    return 'Closed' if change_from_purchase == total_up
-    return 'Insuficient Funds' if change_from_purchase > total_up
-
+    return "Closed" if change_from_purchase == total_up
+    return "Insuficient Funds" if change_from_purchase > total_up
 
     output = {}
+    self.get_change(change_from_purchase)
 
-    cid.each do |nameOfNumber, number|
+    cid.each do |pair|
+      name_of_number = pair[0]
+      number = pair[1]
+      individual_value = CASH_DICTIONARY[name_of_number]
+      accumulated = 0
 
-        while (change_from_purchase >= number) && number.include?(FALSE)
-
-          (output[nameOfNumber] ||= 0)
-          (output[nameOfNumber] += number)
-          (change_from_purchase -= number)
-          (change_from_purchase = change_from_purchase.round(2))
-
-
-        end
-
-
-     end
-
-
-
-
+      while (change_from_purchase >= individual_value && accumulated + individual_value <= number)
+        (output[name_of_number] ||= 0)
+        (output[name_of_number] += individual_value)
+        (change_from_purchase -= individual_value)
+        (change_from_purchase = change_from_purchase.round(2))
+        accumulated += individual_value
+      end
+    end
 
     return output.to_a if change_from_purchase < total_up
-
-
-
-
   end
-
 
   private
 
   def self.total_cash_register_sum(price, cash, cashInDrawer)
-    cashInDrawer.reduce(0){|sum,(name, number)| sum + number}.round(2)
+    cashInDrawer.reduce(0) { |sum, (name, number)| sum + number }.round(2)
   end
 
-  def self.change_needed(itemPrice,cashOnHand,cashInDrawer)
+  def self.change_needed(itemPrice, cashOnHand, cashInDrawer)
     (cashOnHand - itemPrice).round(2)
   end
 
@@ -65,8 +53,7 @@ class CashRegister
     output = {}
     change = calculate
     CASH_DICTIONARY.map do |nameOfNumber, number|
-
-       while change >= number
+      while change >= number
         (output[nameOfNumber] ||= 0)
         (output[nameOfNumber] += number)
         (change -= number)
@@ -75,6 +62,4 @@ class CashRegister
     end
     output.to_a
   end
-
-
 end
